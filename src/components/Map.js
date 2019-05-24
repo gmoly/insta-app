3939403934df9a5a5c84e1cd8dfb03010506a1b7
Mykhailo Grinkov <Mykhailo_Grinkov@epam.com>
2019-05-24 14:03:14 +0300

    add simpler popup listener

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,12 +8,10 @@ class Map extends React.Component {
 
   render() {
     var { flag } = this.props;
-    console.log(flag);
-    var marker = [1].map(e => {
-      if (flag) {
+    var marker = [1].map((e) => {
       return(
       <Marker
-            onadd={ (e) => e.target.openPopup() }
+            ref="marker_0"
             onmouseover={(e) => e.target.openPopup() }
             onmouseout={(e) => e.target.closePopup() }
             position={[51.5, -0.09]} 
@@ -23,20 +21,13 @@ class Map extends React.Component {
             </Popup>
       </Marker>
       );
-    } else {
-      return(
-        <Marker
-              onmouseover={(e) => e.target.openPopup() }
-              onmouseout={(e) => e.target.closePopup() }
-              position={[51.5, -0.09]} 
-              icon= {iconInstagram('5')}>
-              <Popup>
-                 { 'TEST DESCRIPTION 1' }
-              </Popup>
-        </Marker>
-        );
-    }
     })
+
+    if(this.refs.marker_0 && flag) {
+      console.log(this.refs.marker_0.leafletElement.openPopup());
+    }
+   // marker[0].openPopup();
+   
 
     /*var mapMarker = this.props.items.map( element => {
         if (element.location) {
