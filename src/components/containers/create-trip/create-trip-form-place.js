80f8a9c892096781f713be6cf416343899958571
Mykhailo Grinkov <Mykhailo_Grinkov@epam.com>
2019-06-27 17:47:38 +0300

    cnahges in form

---
+++ src/components/containers/create-trip/create-trip-form-place.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect }  from 'react';
+import { Map as LeafletMap,TileLayer,Marker } from 'react-leaflet';
+import {  iconInstagram  } from '../maps/icon';
+import ImageCarousel from '../../images/image-carousel';
+import Collapse from 'react-bootstrap/Collapse';
+
+import '../maps/leaflet-map.css';
+import './create-trip-form.css';
+
+
+export default function placesData({items, updatePlaceData}) {
+
+    return (
+        <div className="accordion" id="accordionPlaces">
+            {
+                items.places.map((item, i) => {
+                    return (
+                         placeData(item, i, updatePlaceData)
+                    );
+                })
+            }
+        </div>
+    )
+}
+   
+function placeData(place, index, updatePlaceData) {
+
+                const [title, useTitle] = useState(place.placeTitle);
+                const [description, useDescription] = useState(place.placeDescription);
+                const [location, useLocation] = useState(place.location);
+                const [media, useMedia] = useState(place.media);
+                const [open, useOpen] = useState(false);
+
+                function handlePositionChanged(e) {
+                    const { lat, lng } = e.latlng;
+                    useLocation({latitude: lat, longitude: lng})
+                }
+
+                
+                useEffect(() => {
+                    const place = {
+                    ...place,
+                    placeTitle: title,
+                    placeDescription: description,
+                    location: location,
+                    media: media.image
+                    }
+                    updatePlaceData(index, place)
+                  }, [title, description, location, media, open]);
+
+
+                return (
+                    <div className="card" key={index} style={{ width: '100%' }}>
+                        <div className="card-header" id={"heading_"+index}>
+                                <h5 className="mb-0">
+                                    <button
+                                        className="btn btn-link"
+                                        type="button"
+                                        onClick={() => useOpen(!open)}
+                                        aria-controls={"collaps_" + index}
+                                        aria-expanded={open}>
+                                        {"#" + (index+1) + " " + location.name } 
+                                    </button>
+                                </h5>
+                            </div>
+                        <Collapse in={open}>
+                            <div id={"collaps_"+index} className="collapse" aria-labelledby={"heading_"+index}>
+                                <div className="card-body"> 
+                                    <div className="form-group carousel-container">
+                                        <ImageCarousel media={ place.media } />
+                                    </div>    
+                                    <div className="form-group">
+                                        <label htmlFor="inputPlaceTitle">Trip title</label>
+                                        <input className="form-control" id="inputPlaceTitle" aria-describedby="titleHelp" 
+                                            placeholder="enter place title" defaultValue={ title } onChange= { (e) => { useTitle(e.target.value) } }/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="inputPlaceDescription1">Place description</label>
+                                        <textarea className="form-control" id="inputPlaceDescription" rows="6" 
+                                        defaultValue={description} onChange= {(e) => { useDescription(e.target.value) }} />
+                                    </div>
+                                    <div className="form-group map-canvas">
+                                            <LeafletMap
+                                                center={[location.latitude, location.longitude]}
+                                                zoom={6}
+                                                minZoom={2}
+                                                maxZoom={17}
+                                                attributionControl={true}
+                                                zoomControl={true}
+                                                doubleClickZoom={true}
+                                                scrollWheelZoom={true}
+                                                dragging={true}
+                                                animate={true}
+                                                easeLinearity={0.35} >
+                                                <TileLayer  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                                                <Marker
+                                                    draggable={true}
+                                                    position={[location.latitude, location.longitude]}
+                                                    icon= {iconInstagram('')}
+                                                    onmouseout={(e) => handlePositionChanged(e) }
+                                                    />
+                                            </LeafletMap>
+                                    </div>
+                                </div>
+                            </div>
+                        </Collapse>
+                    </div>
+                )
+}
--- src/components/containers/create-trip/create-trip-form.js
+++ src/components/containers/create-trip/create-trip-form.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect }  from 'react';
-import { Map as LeafletMap,TileLayer,Marker } from 'react-leaflet';
-import {  iconInstagram  } from '../maps/icon';
-import ImageCarousel from '../../images/image-carousel';
+import React, { useState }  from 'react';
+import  PlacesData from './create-trip-form-place';
 
 import '../maps/leaflet-map.css'
 import './create-trip-form.css'
@@ -34,103 +32,28 @@ export default function TripForm({ items, handleSubmit, removeTrip }) {
     }
 
         return(
-            <form onSubmit={ (e) => submit(e)}>
-                <fieldset>
-                    <legend>Share your trip data here</legend>
-                    {  idFieldData(items.id) }
-                    <div className="form-group">
-                        <label htmlFor="inputTitle">Trip title</label>
-                        <input className="form-control" id="inputTitle" 
-                        aria-describedby="titleHelp" placeholder="enter trip title" 
-                        defaultValue={title} onChange={ e => useTitle(e.target.value) }/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputDescription1">Yor general trip description</label>
-                        <textarea  className="form-control" id="inputDescription" rows="6" 
-                        defaultValue={description} onChange={ e => useDescription(e.target.value)} />
-                    </div>
-                    <ul className="form-group">
-                        {
-                            items.places.map((item, i) => {
-                                return (
-                                <li className="form-group" key={i}>
-                                    { placeData(item, i, updatePlaceData) }
-                                </li> 
-                                );
-                            })
-                        }
-                    </ul>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </fieldset>
-            </form>
-            );
-
-            function placeData(place, index, updatePlaceData) {
-
-                const [title, useTitle] = useState(place.placeTitle);
-                const [description, useDescription] = useState(place.placeDescription);
-                const [location, useLocation] = useState(place.location);
-                const [media, useMedia] = useState(place.media);
-
-                function handlePositionChanged(e) {
-                    const { lat, lng } = e.latlng;
-                    useLocation({latitude: lat, longitude: lng})
-                }
-
-                
-                useEffect(() => {
-                    const place = {
-                    ...place,
-                    placeTitle: title,
-                    placeDescription: description,
-                    location: location,
-                    media: media.image
-                    }
-                    updatePlaceData(index, place)
-                  }, [title, description, location, media]);
-
-
-                return (
-                    <React.Fragment>
-                     <div className="form-group carousel-container">
-                         <ImageCarousel media={ place.media } />
-                     </div>    
-                    <div className="form-group">
-                        <label htmlFor="inputPlaceTitle">Trip title</label>
-                        <input className="form-control" id="inputPlaceTitle" aria-describedby="titleHelp" 
-                            placeholder="enter place title" defaultValue={ title } onChange= { (e) => { useTitle(e.target.value) } }/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputPlaceDescription1">Place description</label>
-                        <textarea className="form-control" id="inputPlaceDescription" rows="6" 
-                        defaultValue={description} onChange= {(e) => { useDescription(e.target.value) }} />
-                    </div>
-                    <div className="form-group map-canvas">
-                            <LeafletMap
-                                center={[location.latitude, location.longitude]}
-                                zoom={6}
-                                minZoom={2}
-                                maxZoom={17}
-                                attributionControl={true}
-                                zoomControl={true}
-                                doubleClickZoom={true}
-                                scrollWheelZoom={true}
-                                dragging={true}
-                                animate={true}
-                                easeLinearity={0.35} >
-                                <TileLayer  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-                                <Marker
-                                    draggable={true}
-                                    position={[location.latitude, location.longitude]}
-                                    icon= {iconInstagram('')}
-                                    onmouseout={(e) => handlePositionChanged(e) }
-                                    />
-                            </LeafletMap>
-        
+            <div class="container w-80">
+                <form onSubmit={ (e) => submit(e)}>
+                    <fieldset>
+                        <legend className="py-5 text-center">Share your trip data here</legend>
+                        {  idFieldData(items.id) }
+                            <div className="form-group col-md-6 mb-3">
+                                <label htmlFor="inputTitle">Trip title</label>
+                                <input className="form-control" id="inputTitle" 
+                                aria-describedby="titleHelp" placeholder="enter trip title" 
+                                defaultValue={title} onChange={ e => useTitle(e.target.value) }/>
+                            </div>
+                        <div className="form-group col-md-10 mb-3">
+                                <label htmlFor="inputDescription1">Yor general trip description</label>
+                                <textarea  className="form-control" id="inputDescription" rows="6" 
+                                defaultValue={description} onChange={ e => useDescription(e.target.value)} />
                         </div>
-                    </React.Fragment>
-                )
-            }
+                        <PlacesData items={items} updatePlaceData={updatePlaceData} />
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </fieldset>
+                </form>
+            </div>
+            );
             
             function idFieldData(id) {
                 if(id) {
